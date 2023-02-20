d55897c76b0fae9f78e80e2660794b4e0c5b81c1
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-02-20 23:01:28 +0530

    navbar html

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ import {
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home'
+import About from "./components/About";
+import Service from "./components/Service";
+import Contact from "./components/Contact";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+      <Navbar />
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/service" element={<Service />}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </Router>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,20 @@ const Navbar = () => {
         <nav className='NavbarItems'>
             <h1 className='navbar-logo'>Trippy</h1>
             <ul className='nav-menu'>
-                <li>
+                <li className='nav-links'>
                     <Link to='/'><i className='fa-solid fa-house'></i> Home</Link>
+                </li>
+                <li className='nav-links'>
+                    <Link to='/about'><i className='fa-solid fa-circle-info'></i>About</Link>
+                </li>
+                <li className='nav-links'>
+                    <Link to='/service'><i className='fa-solid fa-briefcase'></i>Service</Link>
+                </li>
+                <li className='nav-links'>
+                    <Link to='/contact'><i className='fa-solid fa-address-book'></i>Contact</Link>
+                </li>
+                <li className='nav-links-mobile'>
+                    <Link to='/signup'>Sign Up</Link>
                 </li>
             </ul>
         </nav>
