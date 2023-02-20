05d0e63691ccdb3efbd1d136f6b9974eda06b09e
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-02-20 23:57:05 +0530

    navtop css lap finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,21 +8,22 @@ const Navbar = () => {
         <nav className='NavbarItems'>
             <h1 className='navbar-logo'>Trippy</h1>
             <ul className='nav-menu'>
-                <li className='nav-links'>
-                    <Link to='/'><i className='fa-solid fa-house'></i> Home</Link>
+                <li>
+                    <Link className='nav-links' to='/'><i className='fa-solid fa-house'></i> Home</Link>
                 </li>
-                <li className='nav-links'>
-                    <Link to='/about'><i className='fa-solid fa-circle-info'></i>About</Link>
+                <li>
+                    <Link className='nav-links' to='/about'><i className='fa-solid fa-circle-info'></i>About</Link>
                 </li>
-                <li className='nav-links'>
-                    <Link to='/service'><i className='fa-solid fa-briefcase'></i>Service</Link>
+                <li>
+                    <Link className='nav-links' to='/service'><i className='fa-solid fa-briefcase'></i>Service</Link>
                 </li>
-                <li className='nav-links'>
-                    <Link to='/contact'><i className='fa-solid fa-address-book'></i>Contact</Link>
+                <li>
+                    <Link className='nav-links' to='/contact'><i className='fa-solid fa-address-book'></i>Contact</Link>
                 </li>
-                <li className='nav-links-mobile'>
-                    <Link to='/signup'>Sign Up</Link>
+                <li>
+                    <Link className='nav-links-mobile' to='/signup'>Sign Up</Link>
                 </li>
+                <button>Sign Up</button>
             </ul>
         </nav>
     )
