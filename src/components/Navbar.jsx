55781e6a9c53f07f6c8bf6dea173dc61b672ea54
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-02-21 12:28:12 +0530

    navbar style finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 
 const Navbar = () => {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = ()=>{
+        setClicked(!clicked)
+    }
+
     return (
         <nav className='NavbarItems'>
             <h1 className='navbar-logo'>Trippy</h1>
-            <ul className='nav-menu'>
-                <li>
+            <div className='menu-icons'>
+                <i className={clicked ? "fas fa-times" : "fas fa-bars"} onClick={handleClick}></i>
+            </div>
+            <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+                <li className='nav-list-links'>
                     <Link className='nav-links' to='/'><i className='fa-solid fa-house'></i> Home</Link>
                 </li>
-                <li>
+                <li className='nav-list-links'>
                     <Link className='nav-links' to='/about'><i className='fa-solid fa-circle-info'></i>About</Link>
                 </li>
-                <li>
+                <li className='nav-list-links'>
                     <Link className='nav-links' to='/service'><i className='fa-solid fa-briefcase'></i>Service</Link>
                 </li>
-                <li>
+                <li className='nav-list-links'>
                     <Link className='nav-links' to='/contact'><i className='fa-solid fa-address-book'></i>Contact</Link>
                 </li>
                 <li>
